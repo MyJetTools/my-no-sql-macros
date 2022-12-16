415eb6fcb926bf3ca84d508ddbeda96b300293a3
amigin <amigin@gmail.com>
2022-12-16 21:49:33 +0200

    Made ranaming

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,10 @@ extern crate proc_macro;
 mod my_no_sql_entity;
 
 #[proc_macro_derive(
-    BulkSelectDbEntity,
+    MyNoSqlEntity,
     attributes(db_field_name, json, bigint, line_no, sql_type,)
 )]
-pub fn postgres_bulk_select_model(input: TokenStream) -> TokenStream {
+pub fn my_no_sql_entity(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     crate::my_no_sql_entity::generate(&ast)
 }
