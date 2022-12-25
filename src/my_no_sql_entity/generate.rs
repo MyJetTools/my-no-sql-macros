d0f20cef3a182b20ac982d3dd7d00b3f2fa7a0bf
amigin <amigin@gmail.com>
2022-12-25 19:45:20 +0200

    Debug

--- src/my_no_sql_entity/generate.rs
+++ src/my_no_sql_entity/generate.rs
@@ -120,92 +120,27 @@ pub fn generate(attr: TokenStream, input: TokenStream) -> TokenStream {
 }
 
 
-/*
-
-pub fn generate(attr: TokenStream, input: TokenStream) -> TokenStream {
-    let mut result = input.to_string();
-    let pos = find_struct_open(result.as_bytes());
-
-    if pos.is_none() {
-        panic!("Open bracket of the structure is not found");
-    }
-
-    let mut params = get_params(attr.to_string());
-
-    let table_name = params.remove("table_name");
-
-    if table_name.is_none() {
-        panic!("Please specify table_name parameter");
-    }
-
-    let table_name = table_name.unwrap();
-
-    result.insert_str(
-        pos.unwrap() + 1,
-        r#"
-    #[serde(rename = "PartitionKey")]
-    pub partition_key: String,
-    #[serde(rename = "RowKey")]
-    pub row_key: String,
-    #[serde(rename = "TimeStamp")]
-    pub time_stamp: String,"#,
-    );
-
-    let ast: syn::DeriveInput = syn::parse(input).unwrap();
-    let struct_name = ast.ident.to_string();
-
-    result.push_str("impl my_no_sql_server_abstractions::MyNoSqlEntity for ");
-    result.push_str(struct_name.as_str());
-
-    result.push_str("{     const TABLE_NAME: &'static str = ");
-    result.push_str(table_name.as_str());
-
-    result.push_str(
-        r#";
-        fn get_partition_key(&self) -> &str {
-            &self.partition_key
-        }
-
-        fn get_row_key(&self) -> &str {
-            &self.row_key
-        }
-
-        fn get_time_stamp(&self) -> i64 {
-            rust_extensions::date_time::DateTimeAsMicroseconds::parse_iso_string(self.time_stamp.as_str())
-                .unwrap()
-                .unix_microseconds
-        }
-    }"#,
-    );
-
-    result.parse().unwrap()
-}
- */
-fn find_struct_open(src: &[u8]) -> Option<usize> {
-    for i in 0..src.len() {
-        if src[i] == b'{' {
-            return Some(i);
-        }
-    }
-
-    None
-}
 
 
 
 fn get_params(attr: String) -> HashMap<String, String> {
     let count_commas = attr.chars().filter(|&c| c == ',').count();
-    let count_dots = attr.chars().filter(|&c| c == '.').count();
+    let count_semi = attr.chars().filter(|&c| c == ';').count();
 
-    let separator = if count_commas > count_dots { ',' } else { '.' };
+    let separator = if count_commas > count_semi { ',' } else { '.' };
 
     let mut result = HashMap::new();
     for param in attr.split(separator) {
         let mut param = param.split('=');
         let key = param.next().unwrap().trim().to_string();
-        let value = param.next().unwrap().trim().to_string();
+        let mut value = param.next().unwrap().trim();
 
-        result.insert(key, value);
+        if value.starts_with('"'){
+            value = &value[1..value.len()-1];
+        }
+
+
+        result.insert(key, value.to_string());
     }
 
     result
